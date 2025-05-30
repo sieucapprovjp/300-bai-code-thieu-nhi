b922114eb5e875fd7394dff17cce4abbb772c176
Trần Quang Huy <htranquang6@gmail.com>
2025-05-30 22:57:33 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,78 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int nhapso() {
-    int n;
-    do {
-        printf("Nhap so nguyen duong n (>0): ");
-        scanf("%d", &n);
-    } while (n <= 0);
-    return n;
+struct Team {
+    char name[31];
+    int so_tran_thang;
+    int so_tran_thua;
+    int so_tran_hoa;
+    int so_ban_thang;
+    int so_ban_thua;
+    int diem;
+};
+typedef struct Team Team;
+
+void swap(Team *teamA, Team *teamB) {
+    Team temp = *teamA;
+    *teamA = *teamB;
+    *teamB = temp;
 }
 
-int sum(int n) {
-    int tong = 0;
-    int tmp = n;
-    while (tmp > 0) {
-        tong += tmp % 10;
-        tmp /= 10;
-    }
-    printf("Tong cac chu so cua %d la: %d\n", n, tong);
-    return tong;
-}
-
-void liet_ke_uoc(int n) {
-    printf("Cac uoc so cua %d la: ", n);
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            printf("%d ", i);
+void selection(Team a[], int n) {
+    int max_idx;
+    for (int i = 0; i < n - 1; i++) {
+        max_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j].diem > a[max_idx].diem) {
+                max_idx = j;
+            }
+        }
+        if (max_idx != i) {
+            swap(&a[i], &a[max_idx]);
         }
     }
-    printf("\n");
 }
 
-int so_nto(int n) {
-    if (n < 2) return 0;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return 0;
-    }
-    return 1;
-}
-
-void liet_ke_uoc_nto(int n) {
-    printf("Cac uoc so nguyen to cua %d la: ", n);
-    for (int i = 1; i <= n; i++) {
-        if (n % i == 0 && so_nto(i)) {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-}
-void phan_tich_n_la_so_nto(int n) {
-    printf("Phan tich %d thanh cac thua so nguyen to: ", n);
-    for (int i = 2; i * i <= n; i++) {
-        int count = 0;
-        while (n % i == 0) {
-            count++;
-            n /= i;
-        }
-        if (count) {
-            if (count > 1)
-                printf("%d^%d", i, count);
-            else
-                printf("%d", i);
-            if (n > 1) printf(" * ");
-        }
-    }
-    if (n > 1) {
-        printf("%d", n);
-    }
-    printf("\n");
-}
 int main() {
-    int so = nhapso();
-    sum(so);
-    liet_ke_uoc(so);
-    liet_ke_uoc_nto(so);
-    phan_tich_n_la_so_nto(so);
+    int n;
+    scanf("%d", &n);
+    Team doi_bong[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%s", doi_bong[i].name);
+        doi_bong[i].so_tran_thang = 0;
+        doi_bong[i].so_tran_thua = 0;
+        doi_bong[i].so_tran_hoa = 0;
+        doi_bong[i].so_ban_thang = 0;
+        doi_bong[i].so_ban_thua = 0;
+        doi_bong[i].diem = 0;
+    }
+
+    int so_tran = (n * (n - 1)) / 2;
+
+    for (int k = 0; k < so_tran; k++) {
+        int A, B, X, Y;
+        scanf("%d %d %d %d", &A, &B, &X, &Y);
+        A--;
+        B--;
+        doi_bong[A].so_ban_thang += X;
+        doi_bong[A].so_ban_thua += Y;
+        doi_bong[B].so_ban_thang += Y;
+        doi_bong[B].so_ban_thua += X;
+
+        if (X > Y) {
+            doi_bong[A].so_tran_thang++;
+            doi_bong[A].diem += 3;
+            doi_bong[B].so_tran_thua++;
+        } else if (Y > X) {
+            doi_bong[B].so_tran_thang++;
+            doi_bong[B].diem += 3;
+            doi_bong[A].so_tran_thua++;
+        } else {
+            doi_bong[A].so_tran_hoa++;
+            doi_bong[B].so_tran_hoa++;
+            doi_bong[A].diem += 1;
+            doi_bong[B].diem += 1;
+        }
+    }
+    selection(doi_bong, n);
+    for (int i = 0; i < 3 && i < n; i++) {
+        printf("%s %d\n", doi_bong[i].name, doi_bong[i].diem);
+    }
+
+    int r=rand();
+    printf("%d",r/10);
     return 0;
 }
